991d10fa97c39348c933e069ded20766b08d9ede
deepakrattan <mca.deepakrattan@gmail.com>
2021-03-12 12:40:45 +0530

    Data i.e list of tasks is made private and created a getter for accessing the private list.

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,21 +1,31 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:to_do_app_with_provider/models/tasks.dart';
 
 // Step 2 : Provider
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  // Making tasks private using _tasks
+  // Safety of data is done by using a private list
+  List<Task> _tasks = [
     Task(name: 'Buy Milk'),
     Task(name: 'Buy Fruit'),
     Task(name: 'Buy Banana'),
   ];
 
+  // Getter for _tasks
+  // Return a list of tasks that can not be changed
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
   // Add new task
   void addNewTask(String newTaskTitle) {
-    tasks.add(Task(name: newTaskTitle));
+    _tasks.add(Task(name: newTaskTitle));
     // notify to all places which are listening to the TaskData about the change
     notifyListeners();
   }
